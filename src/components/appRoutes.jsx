cd1aa14fb04b2e31da78dc27182a04826f718077
SamiUllah-dev336 <samidev336@gmail.com>
2024-12-28 08:27:30 +0500

    pages

---
+++ src/components/appRoutes.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Dashboard,Users,Ads,Posts,Chat,LogIn,Profile,NotFound } from '../pagesPath'
+
+const appRoutes = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<Dashboard/> } />
+        <Route path='/users' element={<Users/> } />
+        <Route path='/ads' element={<Ads/> } />
+        <Route path='/posts' element={<Posts/> } />
+        <Route path='/chat' element={<Chat/> } />
+        <Route path='/logIn' element={<LogIn/> } />
+        <Route path='/profile' element={<Profile/> } />
+        <Route path='*' element={<NotFound/> } />
+    </Routes>
+  )
+}
+
+export default appRoutes
---
+++ src/pagesPath.jsx
@@ -0,0 +1,8 @@
+export {default as Dashboard} from '../pages/Dashboard'
+export {default as Users} from '../pages/Users';
+export {default as Ads} from '../pages/Ads';
+export {default as Posts} from '../pages/Posts';
+export {default as Chat} from '../pages/Chat';
+export {default as LogIn} from '../pages/LogIn';
+export {default as Profile} from '../pages/Profile'
+export {default as NotFound} from '../pages/NotFound'
