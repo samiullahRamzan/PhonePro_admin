4f1b5e255c39215075f43ce9fdfa4e039b4db8e9
SamiUllah-dev336 <samidev336@gmail.com>
2025-01-12 12:54:45 +0500

    getChats and chat design is completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import React, { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import AppRoutes from "./components/appRoutes";
 import "./App.css";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
 import { LogIn } from "./pagesPath";
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const location = useLocation();
+
+  const isChatScreen = location.pathname === "/chat";
 
   const handleLogin = () => setIsLoggedIn(true);
 
   return isLoggedIn ? (
     <div className="dashboard">
       <Sidebar />
-      <div className="dashboard--content">
+      <div className={`dashboard--content ${isChatScreen ? "no-padding" : ""}`}>
         <AppRoutes />
       </div>
     </div>
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -3,9 +3,13 @@ import "../styles/chat.css";
 import { getAllChats } from "../axios/Chat_axios";
 import { useState } from "react";
 import { getFormattedDate } from "../components/formatDate";
+import { IoSend } from "react-icons/io5";
+import { FaPaperclip } from "react-icons/fa";
 
 const Chat = () => {
   const [chats, setChats] = useState([]);
+  const [chat, setChat] = useState(null);
+  const [message, setMessage] = useState();
   const loginAdmin_id = localStorage.getItem("admin_id");
   console.log("here is login admin id", loginAdmin_id);
 
@@ -23,26 +27,37 @@ const Chat = () => {
     getChats();
   }, []);
 
+  const findProfileImg = (chat) => {
+    return chat.receiver._id == loginAdmin_id
+      ? chat?.sender?.image
+      : chat?.receiver?.image;
+  };
+
+  const findName = (chat) => {
+    return chat.receiver._id == loginAdmin_id
+      ? chat?.sender.fullName || chat?.sender?.shopName
+      : chat?.receiver.fullName || chat?.sender.shopName;
+  };
+
+  const handleChat = async (chat1) => {
+    console.log("here is chat in handle chat..", chat1);
+    setChat(chat1);
+  };
+
   return (
     <div className="main">
       <div className="sidebar-chat">
         <h3>Chats</h3>
 
         {chats.map((chat, index) => (
-          <div className="container" key={index}>
-            <img
-              src={
-                (chat.receiver._id == loginAdmin_id
-                  ? chat?.sender?.image
-                  : chat?.receiver?.image) || "/profile.jpg"
-              }
-            />
+          <div
+            className="container"
+            key={index}
+            onClick={() => handleChat(chat)}
+          >
+            <img src={findProfileImg(chat) || "/profile.jpg"} />
             <div className="Name">
-              <div className="myname">
-                {chat.receiver._id == loginAdmin_id
-                  ? chat?.sender.fullName
-                  : chat?.receiver.fullName}
-              </div>
+              <div>{findName(chat) || "dummy Name"}</div>
               <div className="last-message">
                 {chat.messages[chat.messages.length - 1].message}
               </div>
@@ -51,7 +66,50 @@ const Chat = () => {
           </div>
         ))}
       </div>
-      <div className="chat-view">chat view</div>
+
+      {/* here is a chatview */}
+      {chat && (
+        <div className="chat-view">
+          <div className="heading">
+            {console.log("chat in chatView", chat)}
+            <img src={findProfileImg(chat) || "/profile.jpg"} />
+            <div className="myname">{findName(chat) || "dummy Name"}</div>
+          </div>
+          <div className="middle">
+            <div className="scroll">
+              {chat.messages.map((messages, index) => (
+                <div
+                  className={`message ${
+                    messages.fromType == "User" ? "User" : "admin"
+                  }`}
+                  key={index}
+                >
+                  {console.log(
+                    "here is message fromType in every message",
+                    messages.fromType
+                  )}
+                  {messages.message}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="bottom">
+            <input
+              type="text"
+              id="message"
+              placeholder="Type a message"
+              className="message-input"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+            />
+            <div>
+              <FaPaperclip />
+              <IoSend />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
